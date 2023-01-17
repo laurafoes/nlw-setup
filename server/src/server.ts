a5b592a891ee5e50c9149e97ad3f1f41ba91beb7
laurafoes <foeslaura@gmail.com>
2023-01-17 11:27:40 -0300

    feat: creates backup setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,27 @@
-console.log('hello world')
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+//establishes db connection, enables http methods
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Beber'
+            }
+        }
+    })
+
+    return habits
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP server running!')
+})
